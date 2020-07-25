042c7d24c79aaba234db3b8e9c2a51274d869b2e
Markian Rutkowskyj <i-keep-laughing@myself.com>
2020-07-25 01:35:57 +0100

    First Commit

---
+++ src/main/java/com/sping5WebApp/Sping5WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sping5WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sping5WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sping5WebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sping5WebApp/Sping5WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sping5WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sping5WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
